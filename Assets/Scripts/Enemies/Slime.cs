5e5cb7b94cf17c27e0b47f475bd787384e493d01
Arthuriant <fundito079@gmail.com>
2024-11-04 21:11:56 +0700

    Add Animation

--- Assets/Scripts/Enemies/Slime.cs
+++ Assets/Scripts/Enemies/Slime.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Slime : Enemy
@@ -7,12 +8,14 @@ public class Slime : Enemy
     public float chaseRadius;
     public float attackRadius;
     public Transform homePosition;
+    public Animator anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentState = EnemyState.idle;
         target = GameObject.FindWithTag("Player").transform;
         myRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,12 +31,37 @@ public class Slime : Enemy
             if(currentState == EnemyState.idle || currentState == EnemyState.walk && currentState != EnemyState.stagger)
             {
             Vector3 temp = Vector3.MoveTowards(transform.position,target.position, moveSpeed*Time.deltaTime);
+            changeAnim(temp - transform.position);
             myRigidbody.MovePosition(temp);
             ChangeState(EnemyState.walk); 
+            anim.SetBool("wakeUp", true);
             }
+        }else if (Vector3.Distance(target.position, transform.position)> chaseRadius ){
+            anim.SetBool("wakeUp",false);
         }
     }
 
+    private void SetAnimFloat(Vector2 setVector){
+        anim.SetFloat("moveX" , setVector.x);
+        anim.SetFloat("moveY" , setVector.y);
+
+    }
+
+    private void changeAnim(Vector2 direction){
+        if(Mathf.Abs(direction.x)>MathF.Abs(direction.y)){
+            if (direction.x > 0 ){
+                SetAnimFloat(Vector2.right);
+            }else if (direction.x<0){
+                 SetAnimFloat(Vector2.left);
+            }
+        }else if(Mathf.Abs(direction.x)<MathF.Abs(direction.y)){
+            if (direction.y > 0 ){
+                 SetAnimFloat(Vector2.up);
+            }else if (direction.y<0){
+                 SetAnimFloat(Vector2.down);
+            }
+        }
+    }
     private void ChangeState(EnemyState newState)
     {
         if (currentState != newState){
